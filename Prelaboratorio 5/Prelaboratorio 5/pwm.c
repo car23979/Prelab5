4573c73dbe379876d3d8d0b310b4b9761d535072
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-11 17:16:41 -0600

    Timer2 inicio

--- Prelaboratorio 5/Prelaboratorio 5/pwm.c
+++ Prelaboratorio 5/Prelaboratorio 5/pwm.c
@@ -9,13 +9,15 @@
 #include "pwm.h"
 
 void PWM_Init() {
-	// Configura Timer1 (16 bits) para controlar servos
-	DDRB |= (1 << PINB1) | (1 << PINB2); // Define PB1 y PB2 como salidas
-
+	// Configura Timer1 (16 bits) para servo1
+	DDRB |= (1 << PINB1); // Define PB1 como salida
 	TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11); // Modo PWM Phase Correct, sin inversión
 	TCCR1B = (1 << WGM13) | (1 << CS11); // Prescaler 8, modo PWM con TOP = ICR1
 	ICR1 = 20000; // TOP ? 20,000 ciclos a 16 MHz/8 = 20 ms ? 50 Hz (frecuencia típica para servos)
-
+	
+	// Configura Timer2 (8 bits) para servo2
+	DDRD |= (1 << PIND3);
+	
 	// Configura Timer0 (8 bits) en modo Fast PWM para el control de LED
 	DDRD |= (1 << PIND5); // PD5 como salida
 	TCCR0A = (1 << COM0B1) | (1 << WGM01) | (1 << WGM00); // Fast PWM no invertido
