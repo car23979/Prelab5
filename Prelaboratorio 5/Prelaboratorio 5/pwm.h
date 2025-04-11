999de3904f206978d807a3b143d1461744375ebe
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-11 16:23:07 -0600

    Libreria pwm.h
    se crea la libreria que inicializa los temporizadores para los servos

--- Prelaboratorio 5/Prelaboratorio 5/pwm.h
+++ Prelaboratorio 5/Prelaboratorio 5/pwm.h
@@ -6,12 +6,22 @@
  */ 
 
 // pwm.h
-#ifndef PWM_H
-#define PWM_H
+#ifndef PWM_H_
+#define PWM_H_
 
 #include <avr/io.h>
+#include <stdint.h>
 
+// Inicializa los temporizadores PWM (Timer1 para servos, Timer0 para LED)
 void PWM_Init();
-void PWM_SetDutyCycle(uint16_t duty);
 
-#endif
+// Define la posición del servo 1 conectado a PB1
+void setServo1(uint16_t pulseWidth); // pulseWidth en microsegundos (500–2500 us)
+
+// Define la posición del servo 2 conectado a PB2
+void setServo2(uint16_t pulseWidth);
+
+// Ajusta el brillo del LED conectado a PD5
+void setLED(uint8_t brightness); // 0 = apagado, 255 = brillo máximo
+
+#endif /* PWM_H_ */
