59eacbf7f79124b2b6f859d7323ab134f9ede1ff
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-24 12:35:09 -0600

    Se establecio de momento solo la salida, y el valor que va soltar

--- Prelaboratorio 5/Prelaboratorio 5/timer1_servo2.c
+++ Prelaboratorio 5/Prelaboratorio 5/timer1_servo2.c
@@ -5,17 +5,14 @@
  *  Author: David Carranza
  */ 
 
-#include "pwm2.h"
+#include <avr/io.h>
+#include "timer1_servo2.h"
 
-void PWM2_Init() {
-	// Configura Timer1 (16 bits) para servo1
-	DDRB |= (1 << PINB2); // Define PB1 como salida
-	TCCR1A = (1 << COM1B1) | (1 << WGM11); // Modo PWM Phase Correct, sin inversión
-	TCCR1B = (1 << WGM13) | (1 << CS11);
-	ICR1 = 40000;
-	OCR1B = 3000; // TOP ? 20,000 ciclos a 16 MHz/8 = 20 ms ? 50 Hz (frecuencia típica para servos)	
+void Timer1_Servo2_Init(void) {
+	DDRB |= (1 << PINB2); // PB2 como salida
+	// Solo habilita el pin, Timer1 debe estar configurado antes
 }
 
-void setServo2(uint16_t pulseWidth) {
-	OCR1B = (uint16_t)(pulseWidth * 2); // Aplica pulso al canal B (PB2)
+void SERVO2_set(uint16_t pulso) {
+	OCR1B = pulso;
 }
--- Prelaboratorio 5/Prelaboratorio 5/timer1_servo2.h
+++ Prelaboratorio 5/Prelaboratorio 5/timer1_servo2.h
@@ -5,18 +5,12 @@
  *  Author: David Carranza
  */ 
 
+#ifndef TIMER1_SERVO2_H_
+#define TIMER1_SERVO2_H_
 
-#ifndef PWM2_H_
-#define PWM2_H_
-
-#include <avr/io.h>
 #include <stdint.h>
 
-// Inicializa los temporizadores PWM (Timer1 para servos, Timer0 para LED)
-void PWM2_Init();
+void Timer1_Servo2_Init(void);
+void SERVO2_set(uint16_t pulso);
 
-// Define la posición del servo 2 conectado a PB2
-void setServo2(uint16_t pulseWidth);
-
-
-#endif /* PWM2_H_ */
+#endif
