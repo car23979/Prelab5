d71a1ca0d0edb1e2a4325bcb0c57050681b4f46f
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-24 12:24:59 -0600

    Creacion de una nueva libreria
    pwm_init

---
+++ Prelaboratorio 5/Prelaboratorio 5/pwm_init.c
@@ -0,0 +1,6 @@
+/*
+ * pwm_init.c
+ *
+ * Created: 24/04/2025 12:23:08 p. m.
+ *  Author: Admin
+ */ 
---
+++ Prelaboratorio 5/Prelaboratorio 5/pwm_init.h
@@ -0,0 +1,16 @@
+/*
+ * pwm_init.h
+ *
+ * Created: 24/04/2025 12:23:28 p. m.
+ *  Author: Admin
+ */ 
+
+
+#ifndef PWM_INIT_H_
+#define PWM_INIT_H_
+
+
+void PWM_Init(void);
+
+
+#endif /* PWM_INIT_H_ */
