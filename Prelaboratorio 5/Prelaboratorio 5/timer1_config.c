573e224e720bd5944755ec129014e267295b10ca
car23979 <125841463+car23979@users.noreply.github.com>
2025-04-24 12:36:36 -0600

    Se creo libreria
    Para condiguración del timer1

---
+++ Prelaboratorio 5/Prelaboratorio 5/timer1_config.c
@@ -0,0 +1,6 @@
+/*
+ * timer1_config.c
+ *
+ * Created: 24/04/2025 12:36:11 p. m.
+ *  Author: Admin
+ */ 
---
+++ Prelaboratorio 5/Prelaboratorio 5/timer1_config.h
@@ -0,0 +1,16 @@
+/*
+ * timer1_config.h
+ *
+ * Created: 24/04/2025 12:35:53 p. m.
+ *  Author: Admin
+ */ 
+
+
+#ifndef TIMER1_CONFIG_H_
+#define TIMER1_CONFIG_H_
+
+
+
+
+
+#endif /* TIMER1_CONFIG_H_ */
